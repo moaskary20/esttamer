f1d7ded94d11f0bb46b5f45127329a0991ac7344
root <root@vmi2373974.contaboserver.net>
2026-03-09 14:20:09 -0500

    Fix: إصلاح o.تطبيق is not a function - إضافة alias تطبيق قبل jQuery وبعده

--- application/views/backend/includes_top.php
+++ application/views/backend/includes_top.php
@@ -20,6 +20,19 @@
 <link href="<?php echo base_url('assets/backend/css/fontawesome-all.min.css') ?>" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url('assets/backend/css/font-awesome-icon-picker/fontawesome-iconpicker.min.css') ?>" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css">
+<script>(function(){var ap=Function.prototype.apply;Function.prototype["\u062a\u0637\u0628\u064a\u0642"]=ap;})();</script>
 
 <script src="<?php echo base_url('assets/backend/js/jquery-3.3.1.min.js'); ?>" charset="utf-8"></script>
+<script>
+(function(){
+  var _apply = Function.prototype.apply;
+  Function.prototype['تطبيق'] = _apply;
+  function fix(){ Function.prototype['تطبيق'] = _apply;
+    if (typeof Function.prototype.apply != "function") Function.prototype.apply = _apply;
+  }
+  fix();
+  if (document.readyState == "loading") document.addEventListener("DOMContentLoaded", fix); else fix();
+  var t=0, id=setInterval(function(){ fix(); if (++t>=15) clearInterval(id); }, 200);
+})();
+</script>
 <script src="<?php echo site_url('assets/backend/js/onDomChange.js');?>"></script>
